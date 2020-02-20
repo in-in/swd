0e81f88e55ce206dc8817cdfdfefe43184cab17d
in-in <dmin@tuta.io>
2020-02-20 23:51:00 +0300

    feat(spinner): add spinner component

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import {
 	Avatar,
 	Box,
@@ -8,6 +10,43 @@ import {
 	Heading,
 } from '@primer/components';
 import SwapiService from '../../services/swapi';
+import Spinner from '../Spinner';
+
+const HeroContent = ({ planet }) => {
+	const {
+		diameter, id, name, population, rotationPeriod,
+	} = planet;
+
+	return (
+		<>
+			<Flex flexShrink={0}>
+				<Avatar
+					flexShrink={0}
+					src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+					size={192}
+					alt={`Planet ${name}`}
+				/>
+			</Flex>
+			<Box px={2} bg="blue.2">
+				<Heading as="h4" fontSize={2}>{name}</Heading>
+				<FilterList>
+					<FilterList.Item as="div">
+						<span>Diameter: </span>
+						<span>{diameter}</span>
+					</FilterList.Item>
+					<FilterList.Item as="div">
+						<span>Population: </span>
+						<span>{population}</span>
+					</FilterList.Item>
+					<FilterList.Item as="div">
+						<span>Rotation Period: </span>
+						<span>{rotationPeriod}</span>
+					</FilterList.Item>
+				</FilterList>
+			</Box>
+		</>
+	);
+};
 
 class Hero extends Component {
 	swapiService = new SwapiService();
@@ -16,16 +55,20 @@ class Hero extends Component {
 		super();
 		this.state = {
 			'planet': {},
+			'loading': true,
 		};
 		this.updatePlanet();
 	}
 
 	onPlanetLoaded = (planet) => {
-		this.setState({ planet });
+		this.setState({
+			planet,
+			'loading': false,
+		});
 	}
 
 	updatePlanet() {
-		const id = Math.floor(Math.random() * 20);
+		const id = Math.floor(Math.random() * 18) + 1;
 
 		this.swapiService
 			.getPlanet(id)
@@ -33,42 +76,24 @@ class Hero extends Component {
 	}
 
 	render() {
-		const {
-			'planet': {
-				diameter, id, name, population, rotationPeriod,
-			},
-		} = this.state;
+		const { planet, loading } = this.state;
 
 		return (
 			<Flex bg="blue.1" p={2} flexWrap="wrap">
-				<Flex flexShrink={0}>
-					<Avatar
-						flexShrink={0}
-						src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
-						size={192}
-						alt={`Planet ${name}`}
-					/>
-				</Flex>
-				<Box px={2} bg="blue.2">
-					<Heading as="h4" fontSize={2}>{name}</Heading>
-					<FilterList>
-						<FilterList.Item as="div">
-							<span>Diameter: </span>
-							<span>{diameter}</span>
-						</FilterList.Item>
-						<FilterList.Item as="div">
-							<span>Population: </span>
-							<span>{population}</span>
-						</FilterList.Item>
-						<FilterList.Item as="div">
-							<span>Rotation Period: </span>
-							<span>{rotationPeriod}</span>
-						</FilterList.Item>
-					</FilterList>
-				</Box>
+				{loading ? <Spinner /> : <HeroContent planet={planet} />}
 			</Flex>
 		);
 	}
 }
+
+HeroContent.propTypes = {
+	'planet': PropTypes.shape({
+		'diameter': PropTypes.string,
+		'id': PropTypes.string,
+		'name': PropTypes.string,
+		'population': PropTypes.string,
+		'rotationPeriod': PropTypes.string,
+	}).isRequired,
+};
 
 export default Hero;
