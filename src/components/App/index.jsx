919aab80504454ebcee672ef0f61849fe51d248b
in-in <dmin@tuta.io>
2020-02-22 18:50:41 +0300

    refactor(ant): replace primer with ant

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,34 +1,13 @@
 import React, { Component } from 'react';
-import {
-	createGlobalStyle,
-	StyleSheetManager,
-} from 'styled-components';
-import { BaseStyles } from '@primer/components';
+import { Layout } from 'antd';
 import Hero from '../Hero';
 import List from '../List';
 import 'antd/dist/antd.css';
+import '../../styles/style.css';
 
-
-const ResetStyles = createGlobalStyle`
-	.visually-hidden {
-		position: absolute;
-		width: 1px;
-		height: 1px;
-		overflow: hidden;
-		clip: rect(0, 0, 0, 0);
-		padding: 0;
-		border: 0;
-		white-space: nowrap;
-		pointer-events: none;
-	}
-
-	*:focus {
-		outline: none !important;
-		box-shadow:
-			0 0 0 0.125rem dodgerblue,
-			0 0 0 0.375rem orangered !important;
-	}
-`;
+const {
+	Header, Content, Footer,
+} = Layout;
 
 class App extends Component {
 	constructor() {
@@ -47,13 +26,13 @@ class App extends Component {
 	render() {
 		// const { data } = this.state;
 		return (
-			<StyleSheetManager disableVendorPrefixes>
-				<BaseStyles>
-					<ResetStyles />
-					<Hero />
+			<>
+				<Header>header</Header>
+				<Content><Hero />
 					<List onItemSelected={this.onPersonSelected} />
-				</BaseStyles>
-			</StyleSheetManager>
+				</Content>
+				<Footer>footer</Footer>
+			</>
 		);
 	}
 }
