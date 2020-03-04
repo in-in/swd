2ee062ee95b657d397a44041ff9d8fa39f53081f
in-in <dmin@tuta.io>
2020-03-04 18:12:46 +0300

    refactor(withdata): add error handler

--- src/helpers/withData.jsx
+++ src/helpers/withData.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Spin } from 'antd';
+import ErrorIndicator from '../components/ErrorIndicator';
 
 const withData = (View) => class extends Component {
 		// eslint-disable-next-line react/static-property-placement
@@ -12,25 +13,43 @@ const withData = (View) => class extends Component {
 			super();
 			this.state = {
 				'data': null,
+				'error': false,
+				'loading': true,
 			};
 		}
 
 		componentDidMount() {
 			const { getData } = this.props;
 
+			this.setState({
+				'loading': true,
+				'error': false,
+			});
+
 			getData()
 				.then((data) => {
 					this.setState({
 						data,
+						'loading': false,
+					});
+				})
+				.catch(() => {
+					this.setState({
+						'error': true,
+						'loading': false,
 					});
 				});
 		}
 
 		render() {
-			const { data } = this.state;
+			const { data, loading, error } = this.state;
 
-			if (!data) {
+			if (loading) {
 				return <Spin />;
+			}
+
+			if (error) {
+				return <ErrorIndicator />;
 			}
 
 			return <View {...this.props} data={data} />;
