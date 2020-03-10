0cbe31ed784cd0b1a6ce20059e1ea1a81217259c
in-in <dmin@tuta.io>
2020-03-10 11:47:16 +0300

    refactor(header): convert to functional component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,38 +1,33 @@
-/* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { Layout, Menu, Row } from 'antd';
 
 const { Item } = Menu;
 const { 'Header': AntHeader } = Layout;
 
-class Header extends Component {
-	render() {
-		return (
-			<AntHeader>
-				<Row style={{ 'display': 'flex' }}>
-					<Link to="/">SWD</Link>
-					<Menu mode="horizontal">
-						<Item key="people">
-							<Link to="/people/">People</Link>
-						</Item>
-						<Item key="planets">
-							<Link to="/planets/">Planets</Link>
-						</Item>
-						<Item key="starships">
-							<Link to="/starships/">Starships</Link>
-						</Item>
-						<Item key="login">
-							<Link to="/login">Login</Link>
-						</Item>
-						<Item key="secret">
-							<Link to="/secret">Secret</Link>
-						</Item>
-					</Menu>
-				</Row>
-			</AntHeader>
-		);
-	}
-}
+const Header = () => (
+	<AntHeader>
+		<Row style={{ 'display': 'flex' }}>
+			<Link to="/">SWD</Link>
+			<Menu mode="horizontal">
+				<Item key="people">
+					<Link to="/people/">People</Link>
+				</Item>
+				<Item key="planets">
+					<Link to="/planets/">Planets</Link>
+				</Item>
+				<Item key="starships">
+					<Link to="/starships/">Starships</Link>
+				</Item>
+				<Item key="login">
+					<Link to="/login">Login</Link>
+				</Item>
+				<Item key="secret">
+					<Link to="/secret">Secret</Link>
+				</Item>
+			</Menu>
+		</Row>
+	</AntHeader>
+);
 
 export default Header;
