d8b7739327304c2675aeb3c16c8b393e6b4aecde
in-in <dmin@tuta.io>
2020-03-12 10:02:59 +0300

    feat(dropdownmenu): add DropdownMenu component

---
+++ src/components/DropdownMenu/index.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Menu, Dropdown, Button } from 'antd';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { CaretDownOutlined, CaretUpOutlined } from '@ant-design/icons';
+
+const { Item } = Menu;
+
+const menu = (
+	<Menu>
+		<Item key="people">
+			<Link to="/people/">People</Link>
+		</Item>
+		<Item key="planets">
+			<Link to="/planets/">Planets</Link>
+		</Item>
+		<Item key="starships">
+			<Link to="/starships/">Starships</Link>
+		</Item>
+		<Item key="login">
+			<Link to="/login">Login</Link>
+		</Item>
+		<Item key="secret">
+			<Link to="/secret">Secret</Link>
+		</Item>
+	</Menu>
+);
+
+export class DropdownMenu extends Component {
+	constructor() {
+		super();
+		this.state = {
+			'visible': false,
+		};
+	}
+
+	handleVisibleChange = () => {
+		this.setState(
+			(state) => ({ 'visible': !state.visible }),
+		);
+	};
+
+	render() {
+		const { visible } = this.state;
+		return (
+			<Dropdown
+				overlay={menu}
+				trigger={['click']}
+				placement="bottomRight"
+				onVisibleChange={this.handleVisibleChange}
+			>
+				<Button>
+					Menu {visible ? <CaretUpOutlined /> : <CaretDownOutlined />}
+				</Button>
+			</Dropdown>
+		);
+	}
+}
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,44 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {
-	Layout, Menu, Row, Col,
-} from 'antd';
+import { Layout, Row } from 'antd';
+import { DropdownMenu } from '../DropdownMenu';
 
-const { Item } = Menu;
 const { 'Header': AntHeader } = Layout;
 
 const Header = () => (
 	<AntHeader>
 		<Row justify="space-between" align="middle">
-			<Col xs={24} sm={24} md={3}>
-				<Link to="/">SWD</Link>
-			</Col>
-			<Col xs={0} sm={0} md={16}>
-				<Menu
-					mode="horizontal"
-					theme="dark"
-					style={{
-						'display': 'flex',
-						'justifyContent': 'flex-end',
-					}}
-				>
-					<Item key="people">
-						<Link to="/people/">People</Link>
-					</Item>
-					<Item key="planets">
-						<Link to="/planets/">Planets</Link>
-					</Item>
-					<Item key="starships">
-						<Link to="/starships/">Starships</Link>
-					</Item>
-					<Item key="login">
-						<Link to="/login">Login</Link>
-					</Item>
-					<Item key="secret">
-						<Link to="/secret">Secret</Link>
-					</Item>
-				</Menu>
-			</Col>
+			<Link to="/">SWD</Link>
+			<DropdownMenu />
 		</Row>
 	</AntHeader>
 );
