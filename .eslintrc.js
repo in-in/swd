ee035d7fb50b6af68993f14690f986383caf2805
in-in <dmin@tuta.io>
2020-03-12 09:20:06 +0300

    chore(eslint): update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
 		'no-tabs': 0,
 		'quote-props': ['error', 'always'],
 		'indent': ['error', 'tab', { 'SwitchCase': 1 }],
+		'import/prefer-default-export': 0,
 		'react/jsx-indent-props': ['error', 'tab'],
 		'react/jsx-indent': ['error', 'tab'],
 		'react/jsx-one-expression-per-line': 0,
