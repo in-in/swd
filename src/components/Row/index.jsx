22dd560a0254d708095ff08e544256d9dd8ef865
in-in <dmin@tuta.io>
2020-04-06 09:42:57 +0300

    refactor(row): update gutter spacing

--- src/components/Row/index.jsx
+++ src/components/Row/index.jsx
@@ -3,7 +3,11 @@ import PropTypes from 'prop-types';
 import { Row as AntRow, Col } from 'antd';
 
 export const Row = ({ left, right }) => (
-	<AntRow gutter={16}>
+	<AntRow gutter={[
+		{ 'md': 24 },
+		{ 'xs': 24, 'sm': 24, 'md': 0 },
+	]}
+	>
 		<Col xs={24} md={12}>{left}</Col>
 		<Col xs={24} md={12}>{right}</Col>
 	</AntRow>
